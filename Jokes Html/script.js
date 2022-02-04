98ba26d9d876e61dd73f8cfa900011a3e56ad607
tduncan14 <nbatre1@yahoo.com>
2022-02-04 04:21:25 -0800

    keep moving forward

---
+++ Jokes Html/script.js
@@ -0,0 +1,28 @@
+
+const jokeEl = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
+
+
+
+jokeBtn.addEventListener('click',generateJoke);
+
+
+generateJoke()
+
+
+
+function generateJoke(){
+
+    // const config = {
+
+    // }
+
+    fetch('https://icanhazdadjoke.com', {
+        headers:{
+            'Accept':'application/json'
+        }
+    })
+     .then(res => res.json())
+     .then(data => 
+        jokeEl.innerHTML = data.joke)
+}
