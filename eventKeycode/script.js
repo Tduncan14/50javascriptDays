6ebeae33f1f0e30d0ef907a11fc97ee6b5893d0b
tduncan14 <nbatre1@yahoo.com>
2022-02-05 14:38:00 -0800

    keep moving forward

---
+++ eventKeycode/script.js
@@ -0,0 +1,55 @@
+
+
+
+const insert = document.getElementById('insert');
+
+
+
+
+window.addEventListener('keydown', (e) => {
+
+
+
+    insert.innerHTML = `
+
+    <div class="key">
+    a
+    <small>${e.key}</small>
+ </div>
+
+
+ <div class="key">
+     KeyA
+     <small>${e.keyCode}</small>
+ </div>
+
+
+
+ <div class="key">
+ KeyA
+
+ <small>${e.code}</small>
+ </div>
+
+
+ 
+ <div class="key">
+     Press any key to get the keycode
+     </div>
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+
+
+
+})
